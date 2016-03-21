be127ffb9b6cc8dde46ad70b66c9d550281c3a97
Aaron Rice <adrice727@gmail.com>
2016-03-21 13:22:33 -0700

    Initial setup.

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*
+ * Express Dependencies
+ */
+var express = require('express');
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+/*
+ * Routes
+ */
+app.get('/', (req, res, next) => {
+    res.send('index.html');
+});
+
+/*
+ * Start it up
+ */
+app.listen(process.env.PORT || port);
+console.log('Express started on port ' + port);
