801cb941547cc9491db8e63bc0dee042ea53eb0b
Aaron Rice <adrice727@gmail.com>
2016-03-22 12:48:45 -0700

    Let server make request to broadcast api.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /*
  * Express Dependencies
  */
@@ -7,6 +6,7 @@ var express = require('express');
 var app = express();
 var port = 8000;
 var bodyParser = require('body-parser');
+var axios = require('axios');
 
 app.use(bodyParser.json());
 
@@ -18,6 +18,50 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res, next) => {
     res.send('index.html');
 });
+
+/*
+ * API
+ */
+var apiKey = '';
+var apiSecret = '';
+app.get('/config', (req, res, next) => {
+    res.json({
+        apiKey,
+        apiSecret
+    });
+});
+
+app.get('/broadcast', (req, res, next) => {
+    
+    let url = `https://api.opentok.com/v2/partner/${apiKey}/broadcast`;
+    let sessionId = '2_MX40NTIzMjY3Mn5-MTQ1ODU5MTk4MTI3MH5LWnN0YXhRdHFJRm5lRTZ0ZGwxaG9JVkF-fg';
+
+    let requestConfig = {
+        method: 'post',
+        url: url,
+        data: {
+            sessionId
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'X-TB-PARTNER-AUTH': `${apiKey}:${apiSecret}`,
+            'Access-Control-Allow-Origin': 'http://localhost:8000'
+        }
+    };
+    
+    axios(requestConfig)
+    .then(response => {
+        res.json(response);
+    })
+    .catch(error => {
+        let message = error.data.message;
+        res.status(500);
+        res.send({error:message});
+    })
+
+});
+
+
 
 /*
  * Start it up
--- public/js/api.js
+++ public/js/api.js
@@ -2,77 +2,47 @@
 
 const api = (() => {
 
-    let _config = new Map();
+    // let _config = new Map();
 
-    const setConfig = (config) => {
-        _config.set('apiKey', config.apiKey);
-        _config.set('apiSecret', config.apiSecret);
-    };
+    // const setConfig = (config) => {
+    //     _config.set('apiKey', config.apiKey);
+    //     _config.set('apiSecret', config.apiSecret);
+    // };
 
-    const createBroadcastRequest = () => {
+    // const getConfig = () => {
 
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('X-TB-PARTNER-AUTH', `${_config.apiKey}:${_config.apiSecret}`);
+    //     return new Promise((resolve, reject) => {
 
-        let sessionId = '2_MX40NTIzMjY3Mn5-MTQ1ODU5MTk4MTI3MH5LWnN0YXhRdHFJRm5lRTZ0ZGwxaG9JVkF-fg';
-        let url = `https://api.opentok.com/v2/partner/${_config.apiKey}/broadcast`;
-
-        return new Request(url, {
-            headers
-        });
-
-    };
-
-    const getConfig = () => {
-
-        return new Promise((resolve, reject) => {
-
-            fetch('/config')
-                .then(response => {
-                    return response.json();
-                })
-                .then(config => {
-                    setConfig(config);
-                    resolve();
-                })
-                .catch(err => {
-                    reject(err);
-                });
-        });
+    //         fetch('/config')
+    //             .then(response => {
+    //                 return response.json();
+    //             })
+    //             .then(config => {
+    //                 setConfig(config);
+    //                 resolve();
+    //             })
+    //             .catch(err => {
+    //                 reject(err);
+    //             });
+    //     });
 
 
 
-    };
+    // };
 
     const getBroadcastUrl = (apiKey) => {
 
         return new Promise((resolve, reject) => {
 
-            let getUrl = () => {
-                
-                let request = createBroadcastRequest();
-                
-                fetch(request).then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    console.log('data from broadcast api', data);
-                    resolve(data);
+            fetch('/broadcast')
+                .then(response => response.json())
+                .then(broadcastUrl => {
+                    resolve(broadcastUrl);
                 })
                 .catch(function(err) {
+                    console.error(err);
                     reject(err);
                 });
-                
-            };
-            
-            // Get api config variables from server
-            getConfig().then(response => {
-                getUrl();
-            });
-
-
-
         });
 
     };
