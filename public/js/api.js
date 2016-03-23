5797eacc27c3022908a1749748f7e2374a777978
Aaron Rice <adrice727@gmail.com>
2016-03-23 12:18:22 -0700

    Minor refactor to index and api module.

--- public/js/api.js
+++ public/js/api.js
@@ -2,53 +2,32 @@
 
 const api = (() => {
 
-    // let _config = new Map();
-
-    // const setConfig = (config) => {
-    //     _config.set('apiKey', config.apiKey);
-    //     _config.set('apiSecret', config.apiSecret);
-    // };
-
-    // const getConfig = () => {
-
-    //     return new Promise((resolve, reject) => {
-
-    //         fetch('/config')
-    //             .then(response => {
-    //                 return response.json();
-    //             })
-    //             .then(config => {
-    //                 setConfig(config);
-    //                 resolve();
-    //             })
-    //             .catch(err => {
-    //                 reject(err);
-    //             });
-    //     });
-
-
-
-    // };
-
     const getBroadcastUrl = (apiKey) => {
 
         return new Promise((resolve, reject) => {
 
             fetch('/broadcast')
-                .then(response => response.json())
-                .then(broadcastUrl => {
-                    resolve(broadcastUrl);
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    !!data.error ? reject(data.error.data.message) : resolve(data);
                 })
                 .catch(function(err) {
-                    console.error(err);
                     reject(err);
                 });
         });
 
     };
 
+    const endBroadcast = (id) => {
+
+
+    };
+
     return {
-        getBroadcastUrl
+        getBroadcastUrl,
+        endBroadcast
     };
 
 })();
